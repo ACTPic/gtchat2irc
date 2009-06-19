0ca9f5cd3f77bb61356ff6c9bf476dc21a95300e
Alexander Schremmer <alex AT alexanderweb DOT de>
2009-06-19 15:15:27 +0200

    Fixed url generation, added HTTP 1.0 support, refactored to class.

--- gtchatbridge/gtchatclient.py
+++ gtchatbridge/gtchatclient.py
@@ -4,6 +4,8 @@ import os
 import re
 import urllib2
 import asyncore
+import httplib
+import time
 
 import mechanize
 
@@ -13,46 +15,78 @@ sys.path.append("..")
 from gtchatbridge import config
 
 
-b = mechanize.Browser(
-    factory=mechanize.DefaultFactory(i_want_broken_xhtml_support=True)
-)
-b.set_handle_robots(False)
-b.open(config.url)
-#b.follow_link(nr=0)
-b.select_form(nr=0)
+# generate opener manually to enforce HTTP 1.0
+class HTTP10Connection(httplib.HTTPConnection):
+    _http_vsn = 10
+    _http_vsn_str = 'HTTP/1.0'
 
-b["username"] = config.username 
-b["password"] = config.password
-b["room"] = [config.room]
-b.submit()
-res = b.follow_link(nr=0)
-content = res.read()
+class HTTP10Handler(urllib2.AbstractHTTPHandler):
+    def http10_open(self, req):
+        return self.do_open(HTTP10Connection, req)
 
-idmatch = re.search(r'chat\.pl\?id=(\d+)"', content)
-id = 0
-if idmatch and idmatch.group(1):
-	id = idmatch.group(1)
+    http10_request = urllib2.AbstractHTTPHandler.do_request_
 
-if not id:
-    print "Login fehlgeschlagen." # XXX exception klasse erstellen
-    print content
-    exit()
+HTTP10Opener = urllib2.build_opener(HTTP10Handler)
 
-print "Login erfolgreich, Session: id=" + id
-
-data_url = config.url + "?id=%s&action=receive&dhtml=0" # XXX does not work, why? (cookies?)
-data_socket_urlobj = urllib2.urlopen(data_url)
-data_socket = data_socket_urlobj.fp._sock.fp._sock
 
 class ChatDispatcher(asyncore.file_dispatcher):
+    def set_chatconnector(self, connector):
+        self.connector = connector
+
     def handle_read(self):
-        print "DATA FROM CHAT: ", self.recv(100)
+        print "DATA FROM CHAT: ", self.recv(16 * 1024)
 
     def handle_write(self):
         pass
 
     def handle_close(self):
         self.close()
+        print "Verbindung geschlossen"
 
-dispatcher = ChatDispatcher(data_socket)
-asyncore.loop()
+
+class GTChatConnector:
+    def __init__(self):
+        self.browser = mechanize.Browser(
+            factory=mechanize.DefaultFactory(i_want_broken_xhtml_support=True)
+        )
+        self.browser.set_handle_robots(False)
+        self.do_quit = False
+
+    def login(self):
+        b = self.browser
+        b.open(config.url)
+        b.select_form(nr=0)
+
+        b["username"] = config.username
+        b["password"] = config.password
+        b["room"] = [config.room]
+        b.submit()
+        res = b.follow_link(nr=0)
+        content = res.read()
+
+        idmatch = re.search(r'chat\.pl\?id=(\d+)"', content)
+        if idmatch and idmatch.group(1):
+            session_id = idmatch.group(1)
+        else:
+            session_id = None
+        self.session_id = session_id
+
+        if not session_id:
+            print content
+            raise Exception("Login failed.") # XXX exception klasse erstellen
+
+    def read_data(self):
+        data_url = config.url.replace("http://", "http10://") + "?id=%s&action=receive&dhtml=0" % session_id
+        data_socket_urlobj = HTTP10Opener.open(data_url)
+        data_socket = data_socket_urlobj.fp._sock.fp._sock
+
+        dispatcher = ChatDispatcher(data_socket)
+        dispatcher.set_chatconnector(self)
+        asyncore.loop()
+
+    def run(self):
+        self.login()
+        while not self.do_quit:
+            self.read_data()
+            time.sleep(10)
+
